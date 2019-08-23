cb3e6eba687f07c8fb544426b4626ef143fe667e
KhoaRB <ThanhKhoaIT@gmail.com>
2019-08-23 13:55:05 +0700

    Add basic errors class

--- lib/ninja_van/error.rb
+++ lib/ninja_van/error.rb
@@ -1,9 +1,38 @@
 module NinjaVan
   class Error < StandardError; end
+
+  class RequestError < Error
+    attr_reader :response
+
+    delegate :message,
+      :request_id,
+      :code,
+      :title,
+      to: :error
+
+    def initialize(response)
+      super
+      @response = response
+    end
+
+    def error
+      @error ||= OpenStruct.new(response['error'])
+    end
+  end
+
+  class ResponseError < Error
+    attr_reader :error_data
+    def initialize(error_data)
+      super
+      @error_data = error_data
+    end
+  end
+
   class ConfigError < Error; end
   class ForbiddenError < Error; end
   class ServerError < Error; end
-  class RequestError < Error; end
-  class CreateOrderError < Error; end
   class BadParamsError < Error; end
+
+  class CreateOrderError < ResponseError; end
+  class NotFoundError < ResponseError; end
 end
